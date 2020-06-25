13a50258bdf60b39a4b9c36a60e1c7db22b3d11c
SANDEEP <SANDEEP@Nanna-PC>
2020-06-25 14:17:10 +0530

    sir can u check the code once please.

---
+++ DXC_first/src/feesManagementSystem/Accountant.java
@@ -0,0 +1,27 @@
+package feesManagementSystem;
+
+import java.io.BufferedReader;
+
+public class Accountant {
+
+	public void addStudent(BufferedReader buff)
+	{
+		
+	}
+	public  void viewStudent(BufferedReader buff)
+	{
+		
+	}
+	public void editStudent(BufferedReader buff)
+	{
+	
+	}
+	public  void  dueFees(BufferedReader buff)
+	{
+	
+	}
+	public  void logOut(BufferedReader buff)
+	{
+	
+	}
+}
---
+++ DXC_first/src/feesManagementSystem/Admin.java
@@ -0,0 +1,19 @@
+package feesManagementSystem;
+
+import java.io.BufferedReader;
+
+public class Admin {
+
+	public void addAccount(BufferedReader buff)
+	{
+		
+	}
+	public void viewAccount(BufferedReader buff)
+	{
+		
+	}
+	public void logOut(BufferedReader buff)
+	{
+		
+	}
+}
