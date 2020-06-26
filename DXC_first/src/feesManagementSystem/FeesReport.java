23f45d8a5edff64b91e8e9dad25857ed3d25eb46
SANDEEP <SANDEEP@Nanna-PC>
2020-06-26 14:49:25 +0530

    FeesReport program

--- DXC_first/src/feesManagementSystem/FeesReport.java
+++ DXC_first/src/feesManagementSystem/FeesReport.java
@@ -22,9 +22,7 @@ public class FeesReport {
 	BufferedReader buff = null;
     Accountant acc=null;
     Admin admin=null;
-    Student std=null;
-    
-
+   
 	boolean collegeopen = true;
 	String feesChoice;
 	
@@ -38,12 +36,9 @@ public class FeesReport {
 		System.out.println("Welcome to the fees Management System");
 		System.out.println("Enter Your Choice:");
 		System.out.println("1.Accountant\n2. Admin Login");
-		
-		String mChoice, continueChoice;
-		boolean transaction = true;
 
          String mchoice=mObject.buff.readLine();
-         if(mchoice.equalsIgnoreCase("1"))
+         if(mchoice.equalsIgnoreCase("2"))
          {
         	 System.out.println("Enter Your Name:");
         	 String Uname=mObject.buff.readLine();
@@ -56,7 +51,9 @@ public class FeesReport {
         		 mObject.admin=obj.getInstance(); 
         		 System.out.println("1.Add Accountant\n2.View Accountant\n3.LogOut");
         		 String mychoice=mObject.buff.readLine();
-        		 switch(Integer.parseInt(mychoice))
+        		 do{
+        			 
+         		 switch(Integer.parseInt(mychoice))
         		 {
         		 case 1:
         			     mObject.admin.addAccount(mObject.buff);
@@ -71,37 +68,52 @@ public class FeesReport {
         			 System.out.println("You choose a Invalid Option");
         			 break;
         		 }
-        		 
-        	 }
+        		 System.out.println("Do you want to Continue\n yes\n no");
+     			String choice = mObject.buff.readLine();
+     			if(choice.equalsIgnoreCase("no"))
+    				mObject.collegeopen = false;
+        	 }while (mObject.collegeopen);
          }
-         else
-        	 if(mchoice.equalsIgnoreCase("2"))
-        	 {
-        		System.out.println("Choose Your Choice:");
-        		System.out.println("1.Add Student\n2.View student\n3.Edit Student\n4.Due Fees\n5.LogOut");
-        		String mychoice=mObject.buff.readLine();
-        		switch(Integer.parseInt(mychoice))
-        		{
-        		case 1:
-        			mObject.acc.addStudent(mObject.buff);
-        			break;
-        		case 2:
-        			mObject.acc.viewStudent(mObject.buff);
-        			break;
-        		case 3:
-        			mObject.acc.editStudent(mObject.buff);
-        			break;
-        		case 4:
-        			mObject.acc.dueFees(mObject.buff);
-        			break;
-        		case 5:
-        			mObject.acc.logOut(mObject.buff);
-        			break;
-        		default:
-        			System.out.println("You choose the wrong option");
-        			break;
-        		}
-        	 }
-        	 
-	}   
+         }
+        	 else
+            	 if(mchoice.equalsIgnoreCase("1"))
+            	 {
+            		 UserClass<Accountant> obj=new UserClass<Accountant>();
+            		 obj.create(new Accountant());
+            		 mObject.acc=obj.getInstance();
+            		System.out.println("Choose Your Choice:");
+            		System.out.println("1.Add Student\n2.View student\n3.Edit Student\n4.Due Fees\n5.LogOut");
+            		String mychoice=mObject.buff.readLine();
+            	do {
+            			
+            		switch(Integer.parseInt(mychoice))
+            		{
+            		case 1:
+            			mObject.acc.addStudent(mObject.buff);
+            			break;
+            		case 2:
+            			mObject.acc.viewStudent(mObject.buff);
+            			break;
+            		case 3:
+            			mObject.acc.editStudent(mObject.buff);
+            			break;
+            		case 4:
+            			mObject.acc.dueFees(mObject.buff);
+            			break;
+            		case 5:
+            			mObject.acc.logOut(mObject.buff);
+            			break;
+            		default:
+            			System.out.println("You choose the wrong option");
+            			break;
+            			
+            		} System.out.println("Do you want to Continue\n yes\n no");
+         			String choice = mObject.buff.readLine();
+         			if(choice.equalsIgnoreCase("no"))
+        				mObject.collegeopen = false;
+            	 }while (mObject.collegeopen);
+            	 }
+             	 
+	   
+}
 }
